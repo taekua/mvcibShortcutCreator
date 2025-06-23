fbc3712e75353031f38dc151845ac31d62c6d224
Taekua <taekua@gmail.com>
2025-06-22 19:10:51 -0700

    simple shortcut creator

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openPak = new System.Windows.Forms.OpenFileDialog();
+            this.button1 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // openPak
+            // 
+            this.openPak.FileName = "openFileDialog1";
+            this.openPak.FileOk += new System.ComponentModel.CancelEventHandler(this.openPak_FileOk);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 92);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(303, 76);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Select pak";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(578, 292);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openPak;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public string PakLocation;
+        public string MvciLocation;
+        public string batLocation;
+        public int workflow = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openPak_FileOk(object sender, CancelEventArgs e)
+        {
+            switch (workflow++)
+            {
+                case 0:
+                    this.PakLocation = this.openPak.FileName;
+                    this.button1.Text = "Select MVCI Location";
+                    break;
+            }
+        }
+
+        private void MakeScript()
+        {
+            string script = $@"
+                                del ""{MvciLocation}\Content\Paks\~mods\lk*"" 
+                                del ""{MvciLocation}\Content\Paks\~mods\*ZZ*Balance*"" 
+                                
+                                copy ""{PakLocation}"" ""{MvciLocation}\MVCI\Content\Paks\~mods\""
+                                ""{MvciLocation}\MVCI\Binaries\Win64\MVCI.exe""
+                                pause
+    ";
+            string fileName = new FileInfo(this.openPak.FileName).Name;
+            File.AppendAllText(batLocation + "/" + fileName + ".bat", script);
+            this.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            switch (workflow)
+            {
+                case 0:
+                    this.openPak.ShowDialog();
+                    break;
+                case 1:
+                    if (Directory.Exists("C:\\Program Files (x86)\\Steam\\steamapps\\common\\MARVEL VS. CAPCOM INFINITE"))
+                    {
+                        this.folderBrowserDialog1.SelectedPath = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\MARVEL VS. CAPCOM INFINITE";
+                    }
+                   
+                    DialogResult result = this.folderBrowserDialog1.ShowDialog();
+
+                    if (result == DialogResult.OK)
+                    {
+                        this.MvciLocation = this.folderBrowserDialog1.SelectedPath;
+                        string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                        this.folderBrowserDialog1.SelectedPath = desktopPath;
+                        this.button1.Text = "Select Shortcut Location";
+                        workflow++;
+                    }
+                    break;
+                case 2:
+                    DialogResult result2 = this.folderBrowserDialog1.ShowDialog();
+
+                    if (result2 == DialogResult.OK)
+                    {
+                        this.batLocation = this.folderBrowserDialog1.SelectedPath;
+                        this.button1.Text = "Create Shortcut & exit";
+                        workflow++;
+                    }
+                    break;
+                case 3:
+                    MakeScript();
+                    this.Dispose();
+                    break;
+            }
+            
+        }
+    }
+}
